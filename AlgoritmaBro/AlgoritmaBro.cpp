7b75aaebfae2cf312a8b36e3fb819582d0d0de2e
Synzenn <velriel42@gmail.com>
2024-03-07 11:12:27 +0700

    Add project files.

---
+++ AlgoritmaBro/AlgoritmaBro.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main();
+
+int a[20];				//Deklarasi array a dengan ukuran 20
+int n;					//Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+void input() {		//Prosedur untuk input
+	while (true) {	//Looping
+		cout << "Masukkan banyaknya elemen pada array : ";	// Output ke layar 
+		cin >> n;		// Input dari pengguna 
+		if (n <= 20)	// Jika n kurang dari atau sama dengan 20
+			break;		// Keluar dari loop
+		else {			// Jika n lebih dari 20 
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";	//Output ke layar
+		}
+
+	}
+	cout << endl;								// output ke layar
+	cout << "================" << endl;			// output ke layar
+	cout << "Masukkan Elemen Array" << endl;	// output ke layar
+	cout << "================" << endl;			// output ke layar
+
+	for (int i = 0; i < n; i++) {		// Looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << "; ";	//Output ke layar
+		cin >> a[i];		// input dari pengguna
+	}
+}
+
+void display() {
+
+
+
+}
